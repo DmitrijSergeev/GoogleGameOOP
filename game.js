0f3c446b26ddc51465a29eff39b21860281839b5
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-08-23 12:42:26 +0400

    change game

--- game.js
+++ game.js
@@ -1,9 +1,17 @@
-class Game {
-    test() {
-        return 'game'
+export class Game {
+    #state = GAME_STATUSES.PENDING
+
+    async getStatus(){
+        return this.#state
+    }
+    async start(){
+        this.#state = GAME_STATUSES.IN_PROGRESS
     }
 }
 
-module.exports = {
-    Game,
+export const GAME_STATUSES = {
+    PENDING: 'PENDING',
+    IN_PROGRESS: 'iN-PROGRESS',
+    FINISHED: 'FINISHED'
 }
+
--- game.test.js
+++ game.test.js
@@ -1,8 +1,33 @@
-const { Game } = require('./game')
+import {Game, GAME_STATUSES} from "./game.js";
 
-describe('game tests', () => {
-    it('first test', () => {
+describe('Game', () => {
+    it("let's start",  async () => {
         const game = new Game()
-        expect(game.test()).toBe('game')
+
+        let status = await game.getStatus()
+
+        expect(status).toBe(GAME_STATUSES.PENDING)
+
+        await game.start();
+
+        status = await game.getStatus()
+
+        expect(status).toBe(GAME_STATUSES.IN_PROGRESS)
+
     })
+
+    it('should return correct Google position ' +
+        'that changed every second', async ()=>{
+
+        const game = new Game()
+        await game.start()
+
+        let googlePosition = await game.getGooglePosition()
+        expect(googlePosition).toBeDefined()
+
+        setTimeout( async ()=>{
+            let googlePosition2 = await game.getGooglePosition()
+            expect(googlePosition).toBeDefined()
+        }, 3000 )
+    } )
 })
